c7cdeb4651e86d93b59291af296c57e90e74cd6c
Joyce Yu <joyceyu96@gmail.com>
2023-01-11 00:59:04 -0800

    added redirects file

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
-
+import {Link} from 'react-scroll'
 const Home = () => {
   return (
     <div name='home' className='bg-[#0a192f] w-full h-screen'>
@@ -11,11 +11,13 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Software Engineer.</h2>
         <p className='text-[#8892b0] py-4 max-w-700px'>San Francisco, CA</p>
         <div>
+        <Link to="work" smooth={true} duration={500}> 
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work 
-            <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span>
+                View Work
+                <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span>
             </button>
-        </div>
+        </Link>
+      </div>
       </div>
     </div>
   )
