717d443e9fee5159ea8dd011c387d354e1f58a2a
Joyce Yu <joyceyu96@gmail.com>
2023-02-10 01:22:06 -0800

    updated about section and project photo

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -13,13 +13,15 @@ const About = () => {
             </div>
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                 <div className='sm:text-right text-4xl font-bold'>
-                    <p>Hi! I'm Joyce, please take a look around.</p> 
+                    <p>Hi, I'm Joyce, a full stack software engineer!</p> 
                 </div>
                 <div>
                     <p> I have always been driven by a desire to help people and make a positive impact in the world. This motivation led me to a career in sales, where I honed my skills in communication and problem-solving while consistently meeting and exceeding sales targets. However, as I progressed in my career, I realized that my true passion lies in the world of technology and software development.
-
+                    <br></br>
+                    <br></br>
                     I made the decision to pursue a new path and enrolled in a software engineering bootcamp, where I gained a strong foundation in the fundamentals of JavaScript and Python. From there, I began working on a variety of software development projects, including building full stack web applications for music lovers and automotive dealerships, and converting a monolithic application into microservices.
-                    
+                    <br></br>
+                    <br></br>
                     I am excited to bring my sales experience and technical skills to a career in software engineering, where I can continue to help people and make a positive impact through the innovative solutions I build.</p>
                 </div>
             </div>
