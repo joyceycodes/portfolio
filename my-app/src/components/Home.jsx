63bdbfb527de9dc05034a422ef192e5e241afe67
Joyce Yu <joyceyu96@gmail.com>
2023-03-01 21:21:35 -0800

    added photo and animation

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
+import me from'../assets/me.jpg'
 const Home = () => {
   return (
     <div name='home' className='gradient1 w-full h-screen'>
       {/* container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[1100px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[1100px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+      <div>
         <p className='text-white'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-white'>Joyce Yu</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Software Engineer.</h2>
+
+        <div className='content mb-12'>
+          <h1 className='text-4xl md:text-7xl'>Joyce Yu</h1>
+          <h1 className='text-4xl md:text-7xl'>Joyce Yu</h1>
+        </div>
+        <br></br>
+        <h2 className='text-4xl md:text-7xl text-white'>I'm a</h2>
+        <h2 className='text-4xl md:text-7xl text-white'>Full Stack Software Engineer.</h2>
         <p className='text-white py-4 max-w-700px'>San Francisco, CA</p>
-        <div>
-        <Link to="work" smooth={true} duration={500}> 
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:text-gray-500 hover:-gray-400'>
-                View Work
-                <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span>
-            </button>
-        </Link>
+          <div>
+          <Link to="work" smooth={true} duration={500}> 
+              <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:text-gray-500 hover:-gray-400'>
+                  View Work
+                  <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span>
+              </button>
+          </Link>
+        </div>
+        </div>
+        <div className='rounded-full' style={{backgroundImage: `url(${me})`,backgroundPosition:'0px 510px', backgroundSize:'560px 725px'}}>
+        </div>
       </div>
+
       </div>
     </div>
   )
 }
 
 export default Home
+// <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a</h2>
+// <h2 className='text-4xl sm:text-7xl font-bold text-white'>Full Stack Software Engineer.</h2>
