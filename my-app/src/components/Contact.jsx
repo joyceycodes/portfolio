7a8cee2906db16675823b3a785b48406d874287c
Joyce Yu <joyceyu96@gmail.com>
2023-03-01 21:46:18 -0800

    updated colors for social media contacts

--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-white text-white'>Contact</p>
                 <p className='text-white p-4'>Submit the form below or send me an email at joyceyu96@gmail.com</p>
             </div>
-            <input className='bg-[#e0e0e1] p-2 placeholder-white text-gray-500' type="text" placeholder='Name' name='name'/>
-            <input className='my-4 p-2 bg-[#e0e0e1] placeholder-white text-gray-500' type="email" placeholder='Email' name='email'/>
-            <textarea className='bg-[#e0e0e1] p-2 placeholder-white text-gray-500' name="message" cols="30" rows="10" placeholder='Message'></textarea>
+            <input className='bg-[#e0e0e1] p-2 placeholder-white text-gray-500' aria-label='name' type="text" placeholder='Name' name='name'/>
+            <input className='my-4 p-2 bg-[#e0e0e1] placeholder-white text-gray-500' aria-label='email' type="email" placeholder='Email' name='email'/>
+            <textarea className='bg-[#e0e0e1] p-2 placeholder-white text-gray-500' aria-label='message' name="message" cols="30" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:text-gray-500 hover:border-gray-400 px-3 py-4 my-8 mx-auto flex items-center'>Let's collaborate ◡̈ </button>
         </form>
     </div>
