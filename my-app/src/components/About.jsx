30af1cc99664cdea80965a00b63fe7b68a66a59d
Joyce Yu <joyceyu96@gmail.com>
2023-04-02 20:49:02 -0700

    fixed sections

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-100 md:h-screen gradient2 text-white'>
+    <div name="about" className='w-full h-screen gradient2 text-white '>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
             <div className='sm:text-right pb-8 pl-4'>
